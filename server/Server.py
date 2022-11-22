3029584845cd174c1897f2ecdbea1166214ae362
Brandt Sanche <brandtske@gmail.com>
2022-11-22 13:46:34 -0700

    Added misc functions for encrypting and sending

--- server/Server.py
+++ server/Server.py
@@ -2,6 +2,57 @@ import json
 import socket
 import os,glob, datetime
 import sys
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Util.Padding import pad, unpad
+
+def binary_to_string(bin_str):
+    return bin_str.decode('utf-8')
+
+def string_to_binary(string):
+	return string.encode('utf-8')
+
+# Purpose: creates cipher from public or private key file
+# USE FOR PUBLIC AND PRIVATE KEY CIPHERS
+# Parameters: file name of key
+# Return: generated cipher
+def create_assymetric_cipher(fileName):
+	with open(fileName, "rb") as key_file:
+		key_data = key_file.read()
+	key = RSA.import_key(key_data)
+	cipher = PKCS1_OAEP.new(key)
+	return cipher
+
+# Purpose: pad and encrypt message to be sent
+# Parameters: message string, AES or RSA cipher, required pad bytes
+# Return: encrypted and padded message
+def encrypt_message(message, cipher, padBytes):
+	# encrypt message padded with 256 bits/32 bytes
+	encryptedMessage = cipher.encrypt(pad(message.encode('ascii'), padBytes))
+	return encryptedMessage
+
+# Purpose: decrypt message and remove padding
+# Parameters: encrypted message, AES or RSA cipher and amount of pad bytes to be removed
+# Return: decoded and unpadded message
+def decrypt_message(encryptedMessage, cipher, padBytes):
+	padded_message = cipher.decrypt(encryptedMessage)
+	unpadded_message = unpad(padded_message, padBytes)
+	decoded_message = unpadded_message.decode('ascii')
+	return decoded_message
+
+# Purpose: send message to specified socket
+# Parameters: message, socket to send to
+def sendMessage(message, socketType):
+	socketType.send(message)
+	return
+
+# Purpose: receive message from specified socket
+# Parameters: socket
+# Return: received message
+def receiveMessage(socketType):
+	message = socketType.recv(2048)
+	return message
+	
 
 # Purpose: returns a dictionary of usernames and
 # passwords from user_pass.json
@@ -12,14 +63,13 @@ def read_user_pass():
 	user_json.close()
 	return clients
 
-# Purpose: make directories for clients if they do not exist already.
+# Purpose: make directory for client if they do not exist already.
 # These folders need to exist in order to store emails for respective clients.
-# Parameters: list of client usernames (usernames)
-def make_client_directories(usernames):
-	for username in usernames:
-		# check if path exists before making directory
-		if os.path.exists(username) == False:
-			os.mkdir(username)
+# Parameters: client (username)
+def make_client_directory(username):
+	# check if path exists before making directory
+	if os.path.exists(username) == False:
+		os.mkdir(username)
 	return
 
 def main():
@@ -28,7 +78,6 @@ def main():
 	for username in clients:
 		client_names.append(username)
 	# create folders for client emails
-	make_client_directories(client_names)
 	return
 
 main()
